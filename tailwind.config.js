9b1a493bfd64b80d7474f2c5e86d2eb1328ee3da
Moisés Segura Cedrés <msegurace@uoc.edu>
2023-05-29 16:37:46 +0100

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+}
+
